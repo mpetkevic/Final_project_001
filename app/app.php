df21cadd0a7aabda242b2e6c93ae9238b442eb2f
Miroslav P <miroslav.petkevic@gmail.com>
2018-03-11 15:26:18 +0200

    Final Project

--- app/app.php
+++ app/app.php
@@ -5,16 +5,35 @@ require_once "user_info.php";
 require_once "func.php";
 $user = userExists($db, $email);
 
+
 echo "<hr>Authorize:<br>";
 if($_POST['email'] !== "") {
     if ($user === 0) {
-        userRegistration($db,$name, $email, $phone);
+        $user = userRegistration($db,$name, $email, $phone);
+        echo "CODE IS WORKING";
+        echo "<pre>";
+        var_dump($user);
+        echo "</pre>";
     } else {
         echo "User exists & authorized";
     }
 } else {
     echo "No data for user";
 }
+
+$userId = $user['id'];
+
+
+
+
+$order = orderDetails ($db,$userId, $address, $details);
+
+
+
+$orderCount = ordersCount($db, $userId);
+
+sendMail($user, $order, $orderCount);
+
 
 echo "<hr>";
 echo "<a href='../public_html/index.php'>Back</a>";
--- app/func.php
+++ app/func.php
@@ -8,6 +8,8 @@ function userRegistration($db,$name, $email, $phone)
     $sql = "INSERT INTO users (name, email, phone) VALUES ('".$name."', '".$email."', '".$phone."')";
     $db->query($sql);
     echo "New user registered";
+    return userExists($db,$email);
+
 }
 
 function userExists($db, $email)
@@ -25,6 +27,40 @@ function userExists($db, $email)
     }
 }
 
+function orderDetails ($db,$userId, $address, $details)
+{
+    $sql = "INSERT INTO orders (user_id, user_address, user_details) VALUES ('$userId', '$address', '$details')";
+    $db->query($sql);
+    $sql = "SELECT * FROM orders WHERE 1 ORDER BY id DESC LIMIT 1";
+    $result = $db->query($sql);
+    $data = $result->fetch_assoc();
+    return $data;
+}
 
+function ordersCount ($db, $id)
+{
+    $sql = "SELECT * FROM orders WHERE user_id='$id'";
+    $result = $db->query($sql);
+    $orders = 0;
+    for ($row = 0; $row < $result->num_rows; $row++) {
+        if ($data= $result->fetch_assoc()) {
+            $orders++;
+        }
+    }
+
+    return $orders;
+}
+
+function sendMail ($user, $order, $orderCount) {
+    $to = $user['email'];
+    $subject = "Новый заказ №" . $order['id'];
+    $msg = "Ваш заказ будет доставлен по адресу: " . $order['user_address']. "<br>";
+    $msg .= "DarkBeefBurger за 500 рублей, 1 шт, <br>";
+    $msg .= "Спасибо! Это уже ". $orderCount . " заказ";
+//    echo "Order to: " . $to . " " . $subject . "<br>";
+//    echo $msg;
+    mail($to, $subject, $msg);
+
+}
 
 ?>
